c237f194197286d90db868032e4df710b61b0c5d
piupiunn <ultragalaktikkozmikyokedici@gmail.com>
2022-03-13 01:57:02 +0300

    covid-tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  //https://disease.sh/v3/covid-19/countries
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header"></div>
+
+      <h1>COVID TRACKER</h1>
+      <FormControl className="dropdown">
+        <Select variant="outlined" value="abc">
+          {countries.map((country) => (
+            <MenuItem value={country.value}>{country.name}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </div>
   );
 }
